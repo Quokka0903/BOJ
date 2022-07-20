3ba8fe4a5c0fca510a5bfa9e0fbc1fc3b207b565
이정규 <kue411@naver.com>
2022-07-20 22:29:46 +0900

    220720

--- Bronze/13300.py
+++ Bronze/13300.py
@@ -1,7 +1,7 @@
 import math
 
 N, K = map(int, input().split())
-student = [[0] * 6 for _ in range(2)] #성별 / 학년별
+student = [[0] * 6 for _ in range(2)]
 
 for _ in range(1, N + 1):
     S, G = map(int, input().split())
@@ -12,5 +12,4 @@ for i in student:
     for j in i:
         room += math.ceil(j / K)
 
-print(student)
 print(room)
