dfb5b01dc1f0032ac6db2a2bbd63b57eb2271b40
이정규 <kue411@naver.com>
2022-07-22 23:30:01 +0900

    10163완료

--- Bronze/10163.py
+++ Bronze/10163.py
@@ -1,18 +1,14 @@
-arr = [[0]*1002 for _ in range(1002)]
+arr = [[0]*1001 for _ in range(1001)]
 T = int(input())
-if T == 0 :
-    print(T)
+for t in range(1, T + 1):
+    x, y, xlen, ylen = map(int, input().split())
+    for i in range(y, y + ylen):
+        arr[i][x:(x + xlen)] = [t] * xlen
 
-else :
-    for t in range(1, T + 1):
-        x, y, xlen, ylen = map(int, input().split())
-        for i in range(x, x + xlen):
-            for j in range(y, y + ylen):
-                arr[i][j] = t
-    cnt = [0] * (T + 1)
-    for i in range(1002):
-        for j in range(1002):
-            if arr[i][j]:
-                cnt[arr[i][j]] += 1
-    for i in range(1, T + 1) :
-        print(cnt[i])
+cnt = [0] * (T + 1)
+for i in range(1001):
+    for j in range(1001):
+        if arr[i][j]:
+            cnt[arr[i][j]] += 1
+for i in range(1, T + 1) :
+    print(cnt[i])
