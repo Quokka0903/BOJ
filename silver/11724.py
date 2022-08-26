0c2da3dda258accefc64a907ed54fe71ef771de2
이정규 <kue411@naver.com>
2022-08-26 17:49:08 +0900

    22082^

--- silver/11724.py
+++ silver/11724.py
@@ -1,3 +1,8 @@
+from collections import deque
+import sys
+
+input = sys.stdin.readline
+
 N, M = map(int, input().split())
 nodes = [[] for _ in range(N + 1)]
 
@@ -9,17 +14,17 @@ for _ in range(M):
 visited = [0] * (N + 1)
 union = 0
 for i in range(1, N + 1):
-    q = []
+    q = deque()
     if visited[i] == 0:
         union += 1
         visited[i] = union
         q.append(i)
     while q:
-        s = q.pop(0)
+        s = q.popleft()
         for w in nodes[s]:
             if visited[w] == 0:
                 q.append(w)
                 visited[w] = union
 
-print(visited)
+#print(visited)
 print(union)
