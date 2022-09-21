0a1b07a2fde12314f8304abdb150f978c87a7cfc
이정규 <kue411@naver.com>
2022-09-21 23:26:14 +0900

    220921

---
+++ silver/1389.py
@@ -0,0 +1,32 @@
+import sys
+input = sys.stdin.readline
+
+N, M = map(int, input().split())
+chingu = [[] for _ in range(N + 1)]
+for _ in range(M):
+    a, b = map(int, input().split())
+    chingu[a].append(b)
+    chingu[b].append(a)
+
+print(chingu)
+
+min_hap = N * N
+ans = 0
+for i in range(1, N + 1):
+    visited = [0] * (N + 1)
+    stack = [[i, 0]]
+    while stack:
+        n, cnt = stack.pop()
+        for unit in chingu[n]:
+            if unit == i:
+                continue
+            if visited[unit] == 0 or cnt + 1 < visited[unit]:
+                visited[unit] = cnt + 1
+                stack.append([unit, cnt + 1])
+
+    print(visited)
+    if min_hap > sum(visited):
+        min_hap = sum(visited)
+        ans = i
+
+print(ans)
