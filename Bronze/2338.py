883a2564fcb578ae0eae59d4d28b5b42fdb3950c
이정규 <kue411@naver.com>
2022-10-13 23:59:55 +0900

    221013

---
+++ Bronze/2338.py
@@ -0,0 +1,7 @@
+ans = []
+for _ in range(2):
+    ans.append(int(input()))
+
+print(ans[0] + ans[1])
+print(ans[0] - ans[1])
+print(ans[0] * ans[1])
---
+++ Bronze/2558.py
@@ -0,0 +1,5 @@
+ans = []
+for _ in range(2):
+    ans.append(int(input()))
+
+print(ans[0] + ans[1])
