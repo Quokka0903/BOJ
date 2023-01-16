ecf34ef4aa717d99f6bbe255c0b2a56d69e5496e
이정규 <kue411@naver.com>
2023-01-16 22:53:17 +0900

    230116

---
+++ Bronze/2747_2748_10826.py
@@ -0,0 +1,14 @@
+n = int(input())
+
+f1, f2, f_f = 1, 1, 0
+if n >= 3 :
+    for i in range(3, n + 1) :
+        f_f = f1 + f2
+        f1 = f2
+        f2 = f_f
+    print(f_f)
+
+elif n == 0 :
+    print(0)
+else :
+    print(f1)
