ed8b23959d6b8c57189d7e9a934cbe43b90adb08
gregori <kue411@naver.com>
2023-07-28 23:09:05 +0900

    230728

--- silver/5525.py
+++ silver/5525.py
@@ -1,28 +1,21 @@
 import sys
-
 input = sys.stdin.readline
 
 N = int(input())
 M = int(input())
 S = input()
 
-case = ''
-for _ in range(N):
-    case += 'IO'
-case += 'I'
+answer, i, count = 0, 0, 0
 
-cnt = 0
-i = 2 * N + 1
-while i < M:
-    if S[i - 2*N - 1 : i] == case:
-        cnt += 1
-        while True:
-            if S[i:i+2] == 'OI':
-                cnt += 1
-                i += 2
-            else:
-                break
-        i += 2 * N
-    i += 1
+while i < (M - 1):
+    if S[i:i+3] == 'IOI':
+        i += 2
+        count += 1
+        if count == N:
+            answer += 1
+            count -= 1
+    else:
+        i += 1
+        count = 0
 
-print(cnt)
+print(answer)
